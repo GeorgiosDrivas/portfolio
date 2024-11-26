4a8baaac55968556a42289a6d064efbc5dccbde5
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-11-26 20:42:09 +0200

    Particles packages changes

--- src/particles.tsx
+++ src/particles.tsx
@@ -1,30 +1,12 @@
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { useCallback, useMemo } from "react";
+import Particles from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesComponent = (props: any) => {
-  const [init, setInit] = useState(false);
-  // this should be run only once per application lifetime
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
-      await loadSlim(engine);
-      //await loadBasic(engine);
-    }).then(() => {
-      setInit(true);
-    });
+  // Initialize the tsParticles engine
+  const customInit = useCallback(async (engine: any) => {
+    await loadSlim(engine); // Loads the slim version of tsParticles
   }, []);
-
-  const particlesLoaded = (container: any) => {
-    console.log(container);
-  };
 
   const options: any = useMemo(
     () => ({
@@ -160,7 +142,8 @@ const ParticlesComponent = (props: any) => {
         zIndex: -1,
       }}
     >
-      <Particles id={props.id} init={particlesLoaded} options={options} />
+      {/* Use the customInit function */}
+      <Particles id={props.id} init={customInit} options={options} />
     </div>
   );
 };
