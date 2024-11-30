8db1d9c381ed5cbc1cb6d222a358e2891edcd6d3
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-11-30 15:26:19 +0200

    Skills in alphabetical order

--- src/skills/skills.tsx
+++ src/skills/skills.tsx
@@ -13,57 +13,59 @@ export default function Skills() {
           <div className="row">
             <div className="col-12">
               <div className="skills-wrap d-flex flex-wrap justify-content-center">
-                <SingleSkill image="/skillsIcons/java.svg" title="Java" />
-                <SingleSkill
-                  image="/skillsIcons/javascript.svg"
-                  title="Javascript"
-                />
-                <SingleSkill image="/skillsIcons/react.svg" title="React" />
-                <SingleSkill
-                  image="/skillsIcons/typescript.svg"
-                  title="Typescript"
-                />
-                <SingleSkill image="/skillsIcons/html.svg" title="HTML" />
-                <SingleSkill image="/skillsIcons/css.svg" title="CSS" />
-              </div>
-            </div>
-            <div className="col-12">
-              <div className="mt-3 skills-wrap d-flex flex-wrap justify-content-center">
+                <SingleSkill image="/skillsIcons/angular.svg" title="Angular" />
                 <SingleSkill
                   image="/skillsIcons/bootstrap.svg"
                   title="Bootstrap"
                 />
+                <SingleSkill image="/skillsIcons/css.svg" title="CSS" />
+                <SingleSkill image="/skillsIcons/git.svg" title="GIT" />
+                <SingleSkill image="/skillsIcons/html.svg" title="HTML" />
                 <SingleSkill
-                  image="/skillsIcons/tailwind.svg"
-                  title="Tailwind"
+                  image="/skillsIcons/javascript.svg"
+                  title="Javascript"
                 />
-                <SingleSkill image="/skillsIcons/angular.svg" title="Angular" />
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="mt-3 skills-wrap d-flex flex-wrap justify-content-center">
+                <SingleSkill image="/skillsIcons/java.svg" title="Java" />
+                <SingleSkill image="/skillsIcons/jest.svg" title="Jest" />
+                <SingleSkill image="/skillsIcons/jquery.svg" title="JQuery" />
+
+                <SingleSkill image="/skillsIcons/joomla.svg" title="Joomla" />
+                <SingleSkill image="/skillsIcons/node.svg" title="NodeJs" />
+                <SingleSkill
+                  image="/skillsIcons/postgresql.svg"
+                  title="PostgreSQL"
+                />
+                <SingleSkill image="/skillsIcons/react.svg" title="React" />
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="mt-3 skills-wrap d-flex flex-wrap justify-content-center">
+                <SingleSkill image="/skillsIcons/redux.svg" title="Redux" />
+
                 <SingleSkill
                   image="/skillsIcons/springBoot.svg"
                   title="Spring Boot"
                 />
                 <SingleSkill
-                  image="/skillsIcons/postgresql.svg"
-                  title="PostgreSQL"
+                  image="/skillsIcons/tailwind.svg"
+                  title="Tailwind"
+                />
+                <SingleSkill
+                  image="/skillsIcons/typescript.svg"
+                  title="Typescript"
                 />
               </div>
             </div>
             <div className="col-12">
               <div className="mt-3 skills-wrap d-flex flex-wrap justify-content-center">
-                <SingleSkill image="/skillsIcons/jest.svg" title="Jest" />
-                <SingleSkill image="/skillsIcons/node.svg" title="NodeJs" />
-                <SingleSkill image="/skillsIcons/redux.svg" title="Redux" />
-                <SingleSkill image="/skillsIcons/joomla.svg" title="Joomla" />
                 <SingleSkill
                   image="/skillsIcons/wordpress.svg"
                   title="Wordpress"
                 />
-                <SingleSkill image="/skillsIcons/git.svg" title="GIT" />
-              </div>
-            </div>
-            <div className="col-12">
-              <div className="mt-3 skills-wrap d-flex flex-wrap justify-content-center">
-                <SingleSkill image="/skillsIcons/jquery.svg" title="JQuery" />
               </div>
             </div>
           </div>
