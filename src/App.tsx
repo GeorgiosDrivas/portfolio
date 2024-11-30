d0b5ccfeedd36c263b3cf72338388e21eb4679ea
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-11-30 15:19:35 +0200

    CSS refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import About from "./about";
 import "./App.css";
+import "./responsive.css";
 import Experience from "./experience";
 import Menu from "./menu";
 import Projects from "./projects/projects";
